07fbc90cba94c61ddca42966a6ab45960b111e58
Adit0507 <adi05072002@gmail.com>
2025-09-21 22:27:18 +0530

    implemented concurrent file processing with synchronized term indexing

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -1,6 +1,7 @@
 package indexer
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/Adit0507/wiki-search-engine/internal/storage"
@@ -24,5 +25,50 @@ func NewIndexer(indexPath string, workers int) *Indexer {
 		documents: make(map[uint32]*Document),
 		termIndex: make(map[string][]uint32),
 		storage:   storage.NewDiskStorage(indexPath),
+	}
+}
+
+func (idx *Indexer) ProcessFile(filename string) error{
+	docChan := make(chan *Document, 1000)
+
+	// worker goroutines
+	var wg sync.WaitGroup
+	for i := 0; i < idx.workers; i++ {
+		wg.Add(1)
+		go func ()  {
+			defer wg.Done()
+			
+			for doc := range docChan{
+				idx.addDocument(doc)
+			}
+		}()
+	}
+
+	// pain file
+	parser := NewParser(docChan)
+	err := parser.ParseFile(filename)
+	close(docChan)
+	wg.Wait()
+
+	return err
+}
+
+func (idx *Indexer) addDocument(doc *Document){
+	idx.mutex.Lock()
+	defer idx.mutex.Unlock()
+
+	idx.documents[doc.ID]= doc
+	idx.docCount++
+
+	for term := range doc.Terms{
+		if _, exists := idx.termIndex[term]; !exists{
+			idx.termIndex[term]= make([]uint32, 0)
+		}
+
+		idx.termIndex[term] = append(idx.termIndex[term], doc.ID)
+	}
+
+	if idx.docCount%1000 == 0{
+		fmt.Printf("Processed %d documents... \n", idx.docCount)
 	}
 }
