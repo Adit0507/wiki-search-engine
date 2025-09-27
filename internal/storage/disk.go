9875bb04adfe4a4e5c53e189281cee56a0a3a052
Adit0507 <adi05072002@gmail.com>
2025-09-27 12:45:27 +0530

    saveDocuments to serialize and store documents & saveTermIndex to persit term index to disk

--- internal/storage/disk.go
+++ internal/storage/disk.go
@@ -15,3 +15,25 @@ type DiskStorage struct {
 func NewDiskStorage(indexPath string) *DiskStorage {
 	return &DiskStorage{indexPath: indexPath}
 }
+
+func (ds *DiskStorage) SaveDocuments(documents map[uint32] *models.Document) error {
+	file, err := os.Create(filepath.Join(ds.indexPath, "documents.gob"))
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+    
+    encoder := gob.NewEncoder(file)
+    return encoder.Encode(documents)
+}
+
+func (ds *DiskStorage) SaveTermIndex(termIndex map[string][] uint32) error {
+	file, err := os.Create(filepath.Join(ds.indexPath, "terms.gob"))
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	return encoder.Encode(termIndex)
+}
